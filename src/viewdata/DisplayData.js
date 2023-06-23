b0f232438b089ef2ee4295545af48a09e838f15d
X-workzDev01 <x-workzdev01>
2023-06-23 19:07:05 +0530

    working in front end check with back-end

--- src/viewdata/DisplayData.js
+++ src/viewdata/DisplayData.js
@@ -1,86 +1,81 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { AgGridReact } from 'ag-grid-react';
 import { useMemo } from 'react';
 import 'ag-grid-enterprise';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import Header from '../component/Header';
-import { Grid, GridOptionsService } from 'ag-grid-enterprise';
+import { Urlconstant } from '../constant/Urlconstant';
 
 export default function DisplayData() {
+
     const [rowData, setRowData] = useState();
-    const [totalRowCount, setTotalRowCount] = useState(100)
+    const [totalRowCount, setTotalRowCount] = useState()
+    const [gridApi, setGridApi] = useState(null);
     const [gridOptions, setGridOptions] = useState({
-        columnDefs: [
-            { field: 'studentName' },
-            { field: 'email' },
-            { field: 'contactNumber' },
-            { field: 'address' }
-        ],
-        defaultColDef: useMemo(() => {
-            return {
-                editable: true,
-                sortable: true,
-                filter: true,
-                resizable: true,
-                flex: 1,
-                floatingFilter: true
-            };
-        }, []),
-        
-        pagination: true,
-        paginationPageSize: 10,
-        cacheBlockSize: 10,
-        serverSideDatasource:true,
-        rowModelType: 'serverSide',
-        onGridReady: (params) => {
-            params.api.setServerSideDatasource(createServerSideDatasource());
-        },
     });
+    const defaultColDef = useMemo(() => {
+        return {
+            editable: true,
+            sortable: true,
+            filter: true,
+            resizable: true,
+            flex: 1,
+            floatingFilter: true
+        };
+    }, [])
+    const columnDefs = [
+        { field: 'studentName' },
+        { field: 'email' },
+        { field: 'contactNumber' },
+        { field: 'address' }
+    ];
+    const onGridReady = (params) => {
+        setGridApi(params.api);
+        params.api.setServerSideDatasource(createServerSideDatasource());
 
-
+    }
     const createServerSideDatasource = () => {
-        let stopLoading = false;
         return {
             getRows: (params) => {
                 const { startRow, endRow } = params.request;
-                console.log(startRow,endRow)
-                if (stopLoading) {
-                    return;
-                  }
-                setTimeout(() => {
-                const response = axios.get(`http://localhost:8080/connection/trainees/page?startIndex=${startRow}&endIndex=${endRow}`, {
+                axios.get(Urlconstant.url + `utils/page?startIndex=${startRow}&endIndex=${endRow}`, {
                     headers: {
-                        sheetId: "1WiZVpFrIsl_Wf_mpAG8LV-ObF2Gmwb8Wjw9Bev6qmY4",
+                        'spreadsheetId': Urlconstant.spreadsheetId
                     }
                 }).then((response) => {
-                    const data = response.data;
-                    setTotalRowCount(response.totalRowCount);
-                    setRowData(response.data)
-                    params.successCallback(data, totalRowCount);
-                    if (totalRowCount <= endRow) {
-                        stopLoading = true; // Set the flag to stop further loading
-                      }
+                    console.log(response.data.totalRowCount)
+                    setTotalRowCount(response.data.totalRowCount);
+                    params.successCallback(response.data,response.data.totalRowCount);
                 }).catch((error) => {
-
                     params.failCallback();
-                    console.error('Error fetching data:', error);
                 });
-            },1000);
+
             }
 
         }
-
     }
     return (
         <div>
             <Header />
             <h3>grid view</h3>
+            <h3>Trainee Details</h3>
             <div className="ag-theme-alpine" id="agGrid">
                 <AgGridReact
-                    gridOptions={gridOptions}
-                    rowData= {rowData}
+                    //                   gridOptions={gridOptions}
+                    columnDefs={columnDefs}
+                    pagination={true}
+                    paginationPageSize={10}
+                    cacheBlockSize={10}
+                    serverSideDatasource={true}
+                    animateRows={true}
+                    rowModelType='serverSide'
+                    paginationOverflowSize={10}
+                    onGridReady={onGridReady}
+                    maxConcurrentDatasourceRequests={1}
+                    defaultColDef={defaultColDef}
+
                 />
             </div>
         </div>
