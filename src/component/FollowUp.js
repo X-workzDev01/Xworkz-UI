4b593bc30e5bbd201c0cfa7e0135cf9cd6ce5494
X-workzDev01 <x-workzdev01>
2023-08-22 15:59:59 +0530

    working

--- src/component/FollowUp.js
+++ src/component/FollowUp.js
@@ -7,120 +7,122 @@ import EditFollowUp from './EditFollowUp';
 
 export default function FollowUp() {
   const [isModalOpen, setModalOpen] = React.useState(false);
-    const [editedRowData, setEditedRowData] = React.useState(null);
-    const [dropdown, setDropDown] = useState({
-        status: [],
+  const [editedRowData, setEditedRowData] = React.useState(null);
+  const [dropdown, setDropDown] = useState({
+    status: [],
+  });
+
+  const initialPageSize = 10;
+  const [paginationModel, setPaginationModel] = useState({
+    page: 0,
+    pageSize: initialPageSize,
+  });
+  const [gridData, setGridData] = useState({
+    rows: [],
+    rowCount: 0,
+  });
+  const [loading, setLoading] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  useEffect(() => {
+    getDropDown();
+    setLoading(true);
+    searchServerRows(paginationModel.page, paginationModel.pageSize)
+
+      .then((newGridData) => {
+        setGridData((prevGridData) => ({
+          rows: prevGridData.rows.concat(newGridData.rows),
+          rowCount: newGridData.rowCount,
+        }));
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setGridData({ rows: [], rowCount: 0 });
+        setLoading(false);
       });
+  }, [paginationModel.page, paginationModel.pageSize, searchValue]);
 
-    const initialPageSize = 10;
-    const [paginationModel, setPaginationModel] = useState({
-      page: 0,
-      pageSize: initialPageSize,
-    });
-    const [gridData, setGridData] = useState({
-      rows: [],
-      rowCount: 0,
-    });
-    const [loading, setLoading] = useState(false);
-    const [searchValue, setSearchValue] = useState('');
-    useEffect(() => {
-        getDropDown();
-        setLoading(true);
-        searchServerRows( paginationModel.page, paginationModel.pageSize)
-        
-          .then((newGridData) => {
-            setGridData(newGridData);
-            setLoading(false);
-          })
-          .catch((error) => {
-            console.error('Error fetching data:', error);
-            setGridData({ rows: [], rowCount: 0 });
-            setLoading(false);
-          });
-      }, [paginationModel.page, paginationModel.pageSize, searchValue]);
+  const handleSearchClick = () => {
+    // Reset the pagination model to initial state
+    setPaginationModel({ page: 0, pageSize: initialPageSize });
+  };
 
-      const handleSearchClick = () => {
-        // Reset the pagination model to initial state
-        setPaginationModel({ page: 0, pageSize: initialPageSize });
-      };
+  function searchServerRows(page, pageSize) {
+    const startingIndex = page * pageSize;
+    console.log('Loading server rows with page:', page, 'pageSize:', pageSize, 'status:', searchValue);
+    const spreadsheetId = Urlconstant.spreadsheetId; // Replace this with the actual spreadsheet ID
 
-      function searchServerRows(page, pageSize) {
-        const startingIndex = page*pageSize; 
-        console.log('Loading server rows with page:', page, 'pageSize:', pageSize, 'status:', searchValue);
-        const spreadsheetId = Urlconstant.spreadsheetId; // Replace this with the actual spreadsheet ID
-    
-        const apiUrl = Urlconstant.url + `api/followUp?startingIndex=${startingIndex}&maxRows=10&status=${searchValue}`;
-    
-        const requestOptions = {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            spreadsheetId: spreadsheetId,
-          },
-        };
-        return new Promise((resolve) => {
-          fetch(apiUrl, requestOptions)
-            .then((response) => response.json())
-            .then((data) => {
-              console.log('Received data from server:', data);
-              resolve({
-                rows: gridData.rows.concat(
-                  data.followUpData.map((row) => ({ id: row.id.toString(), ...row }))
-                ),
-                rowCount: data.size,// Set rowCount to the total number of rows (size) in the dataset
-              },1000);
-            })
-            .catch((error) => {
-              console.error('Error fetching data:', error);
-              resolve({ rows: [], rowCount: 0 }); // Return an empty dataset in case of an error
-            });
-        });
-      };
+    const apiUrl = Urlconstant.url + `api/followUp?startingIndex=${startingIndex}&maxRows=10&status=${searchValue}`;
 
-
-    
-
-      const handleEditClick = (row) => {
-        setEditedRowData(row);
-        setModalOpen(true);
-      };
-
-      const handleSaveClick = () => {
-        // Perform save operation here with editedRowData
-        console.log('Edited Data:', editedRowData);
-        // After saving, you may want to update the grid data or reload the data to reflect the changes
-        setModalOpen(false);
-      };
-    
-      const getDropDown = () => {
-        axios.get(Urlconstant.url + 'utils/dropdown', {
-          headers: {
-            'spreadsheetId': Urlconstant.spreadsheetId
-          }
-        }).then(response => {
-          setDropDown(response.data)
-        }).catch(error => {
-          console.log(error);
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        spreadsheetId: spreadsheetId,
+      },
+    };
+    return new Promise((resolve, reject) => {
+      fetch(apiUrl, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('Received data from server:', data);
+          console.log(data.followUpData)
+          const newGridData = {
+            rows: data.followUpData.map((row) => ({ id: row.id.toString(), ...row })),
+            rowCount: data.size,
+          };
+          resolve(newGridData);
         })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+          reject(error); // Reject the promise on error
+        });
+    });
+  }
+
+
+
+  const handleEditClick = (row) => {
+    setEditedRowData(row);
+    setModalOpen(true);
+  };
+
+  const handleSaveClick = () => {
+    // Perform save operation here with editedRowData
+    console.log('Edited Data:', editedRowData);
+    // After saving, you may want to update the grid data or reload the data to reflect the changes
+    setModalOpen(false);
+  };
+
+  const getDropDown = () => {
+    axios.get(Urlconstant.url + 'utils/dropdown', {
+      headers: {
+        'spreadsheetId': Urlconstant.spreadsheetId
       }
-      const handleInputChange = (e) => {
-        setSearchValue(e.target.value);
-        console.log(e.target.value)
-      };
+    }).then(response => {
+      setDropDown(response.data)
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+  const handleInputChange = (e) => {
+    setSearchValue(e.target.value);
+    console.log(e.target.value)
+  };
 
   return (
     <div>
-        <h2>VeiwFollowUp</h2>
-        <h2>FollowUp List</h2>
-        <div className="search" style={{ marginTop: '50px', display: 'flex', alignItems: 'center' }}>
-       <Select name="statusValues"
-         onChange={handleInputChange}
+      <h2>VeiwFollowUp</h2>
+      <h2>FollowUp List</h2>
+      <div className="search" style={{ marginTop: '50px', display: 'flex', alignItems: 'center', width: "200px" }}>
+        <Select name="statusValues"
+          onChange={handleInputChange}
           value={searchValue}
-          fullWidth
           required
           id="outlined-basic"
           variant="outlined"
           sx={{
+            width: '200px',
             marginRight: '10px',
             p: '4px', // Adjust padding for a smaller size
             fontSize: '12px', // Adjust font size for a smaller size
@@ -131,11 +133,11 @@ export default function FollowUp() {
           ))}
 
         </Select>
-       
+
       </div>
       <div style={{ height: '650px', width: '100%' }}>
-      <DataGrid
-           columns={[
+        <DataGrid
+          columns={[
             { headerName: 'ID', field: 'id', flex: 1 },
             { field: 'traineeName', headerName: 'Trainee Name', flex: 1, valueGetter: (params) => params.row.basicInfo.traineeName },
             { field: 'email', headerName: 'Email', flex: 1, valueGetter: (params) => params.row.basicInfo.email },
@@ -166,13 +168,13 @@ export default function FollowUp() {
 
         />
         <EditFollowUp
-        open={isModalOpen}
-        handleClose={() => setModalOpen(false)}
-        rowData={editedRowData}
-        setRowData={setEditedRowData}
-        handleSaveClick={handleSaveClick}
-      />
-        </div>
+          open={isModalOpen}
+          handleClose={() => setModalOpen(false)}
+          rowData={editedRowData}
+          setRowData={setEditedRowData}
+          handleSaveClick={handleSaveClick}
+        />
+      </div>
     </div>
   )
 }
