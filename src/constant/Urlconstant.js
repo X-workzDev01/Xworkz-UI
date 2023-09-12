b10922f06896a364f31f6a4e2c5a4d83bfe97b98
X-workzDev01 <x-workzdev01>
2023-09-12 09:52:30 +0530

    added

--- src/constant/Urlconstant.js
+++ src/constant/Urlconstant.js
@@ -1,11 +1,6 @@
 export const Urlconstant = {
 
-<<<<<<< Updated upstream
- //url:'https://ombn.in/Dream/',
-=======
-//url:'https://ombn.in/Dream/',
-
->>>>>>> Stashed changes
- url: 'http://localhost:8081/',
+//  url:'https://ombn.in/Dream/',
+url: 'http://localhost:8081/',
     spreadsheetId: '1p3G4et36vkzSDs3W63cj6qnUFEWljLos2HHXIZd78Gg'
 }
