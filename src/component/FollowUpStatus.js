164073d5065c9f7a278fe6d0608bf2e119e5cc34
X-workzDev01 <x-workzdev01>
2023-09-23 16:26:12 +0530

    updated the changes

--- src/component/FollowUpStatus.js
+++ src/component/FollowUpStatus.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect } from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -17,48 +16,12 @@ import TimePicker from 'react-time-picker';
 const fieldStyle = { margin: '20px' };
 
 const FollowUpStatus = ({ open, handleClose, rowData }) => {
-    const [isConfirming, setIsConfirming] = React.useState(false);
-    const [editedData, setEditedData] = React.useState({ ...rowData });
-    const [loading, setLoading] = React.useState(false);
-    const [responseMessage, setResponseMessage] = React.useState('');
-    const [snackbarOpen, setSnackbarOpen] = React.useState(false);
-    const [dropdownData, setDropdownData] = React.useState([]);
-
-=======
-import React, { useEffect } from "react";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogTitle from "@mui/material/DialogTitle";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import axios from "axios";
-import Snackbar from "@mui/material/Snackbar";
-import CircularProgress from "@mui/material/CircularProgress";
-import { Urlconstant } from "../constant/Urlconstant";
-import {
-  FormControl,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  MenuItem,
-  Select,
-} from "@mui/material";
-import { GridCloseIcon } from "@mui/x-data-grid";
-import AdapterDateFns from "@mui/lab/AdapterDateFns";
-import { LocalizationProvider, TimePicker } from "@mui/lab";
-
-const fieldStyle = { margin: "20px" };
-
-const FollowUpStatus = ({ open, handleClose, rowData }) => {
   const [isConfirming, setIsConfirming] = React.useState(false);
   const [editedData, setEditedData] = React.useState({ ...rowData });
   const [loading, setLoading] = React.useState(false);
-  const [responseMessage, setResponseMessage] = React.useState("");
+  const [responseMessage, setResponseMessage] = React.useState('');
   const [snackbarOpen, setSnackbarOpen] = React.useState(false);
   const [dropdownData, setDropdownData] = React.useState([]);
-  const [isButtonDisabled, setButtonDisabled] = React.useState(false);
->>>>>>> 3ec4787235d7dbc41716712a5188a6fe167c9b20
 
   const getCurrentDate = () => {
     const now = new Date();
@@ -68,15 +31,6 @@ const FollowUpStatus = ({ open, handleClose, rowData }) => {
     return `${year}-${month}-${day}`;
   };
 
-<<<<<<< HEAD
-    useEffect(() => {
-        axios.get(Urlconstant.url + 'utils/dropdown', {
-            headers: {
-                'spreadsheetId': Urlconstant.spreadsheetId
-            }
-        }).then((response) => {
-            setDropdownData(response.data);
-=======
   useEffect(() => {
     // Fetch your dropdown data from the API here
     axios
@@ -113,12 +67,15 @@ const FollowUpStatus = ({ open, handleClose, rowData }) => {
     setIsConfirming(true);
     setSnackbarOpen(false);
   };
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+    handleClose();
+  };
   const attemtedUser = sessionStorage.getItem("userId");
 
   const handleSaveClick = () => {
     if (isConfirming) {
       setLoading(true);
-      setButtonDisabled(true);
       const statusDto = {
         ...editedData,
         attemptedBy: attemtedUser,
@@ -129,7 +86,6 @@ const FollowUpStatus = ({ open, handleClose, rowData }) => {
             "Content-Type": "application/json",
             spreadsheetId: Urlconstant.spreadsheetId,
           },
->>>>>>> 3ec4787235d7dbc41716712a5188a6fe167c9b20
         })
         .then((response) => {
           console.log("Update success:", response);
@@ -146,46 +102,24 @@ const FollowUpStatus = ({ open, handleClose, rowData }) => {
           setSnackbarOpen(true);
         });
 
-<<<<<<< HEAD
-    React.useEffect(() => {
-        setEditedData(rowData);
-    }, [rowData]);
-
-    if (!rowData) {
-        return null;
-=======
-      axios.post(Urlconstant.url + `registerAttendance`, statusDto, {
-       
-      });
->>>>>>> 3ec4787235d7dbc41716712a5188a6fe167c9b20
     }
-    setIsConfirming(false);
   };
-
-  const handleSnackbarClose = () => {
-    setSnackbarOpen(false);
-    handleClose();
-  };
-
-  const handleConfirmBoxClose = () => {
-    setIsConfirming(false);
-  };
-
   return (
     <Dialog open={open} onClose={handleClose} fullWidth maxWidth="md">
-      <DialogTitle>
-        Add to Follow Up
+      <DialogTitle>Add to Follow Up
+
         <IconButton
           color="inherit"
           onClick={handleClose}
           edge="start"
           aria-label="close"
-          style={{ position: "absolute", right: "8px", top: "8px" }}
+          style={{ position: 'absolute', right: '8px', top: '8px' }}
         >
           <GridCloseIcon />
         </IconButton>
       </DialogTitle>
       <DialogContent>
+
         <TextField
           label="Email"
           name="basicInfo.email"
@@ -216,6 +150,7 @@ const FollowUpStatus = ({ open, handleClose, rowData }) => {
           InputProps={{
             readOnly: true,
           }}
+
         />
         <FormControl>
           <InputLabel id="demo-simple-select-label">Attempt Status</InputLabel>
@@ -228,38 +163,35 @@ const FollowUpStatus = ({ open, handleClose, rowData }) => {
             defaultValue={rowData.attemptStatus}
             variant="outlined"
             sx={{
-              marginRight: "20px",
-              width: "200px", // Adjust padding for a smaller size
-              fontSize: "20px", // Adjust font size for a smaller size
+              marginRight: '20px',
+              width: '200px',
+              fontSize: '20px',
             }}
           >
             {dropdownData.status.map((item, index) => (
-              <MenuItem value={item} key={index}>
-                {item}
-              </MenuItem>
+              <MenuItem value={item} key={index}>{item}</MenuItem>
             ))}
+
           </Select>
         </FormControl>
 
-        <TextField
-          label="Comments"
-          name="comments"
-          defaultValue={rowData.comments}
-          onChange={handleInputChange}
-          style={fieldStyle}
-          rows={4}
-        />
-        <TextField
-          label="Call Duration"
-          name="callDuration"
-          defaultValue={rowData.callDuration}
-          placeholder="hh:mm:ss"
-          onChange={handleInputChange}
-          style={fieldStyle}
-        />
 
-        <TextField
-          type="date"
+        <ReactInputMask
+          mask="99:99:99" // Define the mask pattern for hh:mm:ss
+          value={rowData.callDuration}
+          onChange={handleInputChange}
+        >
+          {() => (
+            <TextField
+              label="Call Duration"
+              name="callDuration"
+              placeholder="hh:mm:ss" // Update the placeholder here
+              variant="outlined"
+            />
+          )}
+        </ReactInputMask>
+
+        <TextField type="date"
           label="Call Back Date"
           name="callBack"
           defaultValue={rowData.callBack}
@@ -269,10 +201,10 @@ const FollowUpStatus = ({ open, handleClose, rowData }) => {
             shrink: true,
           }}
           inputProps={{
-            min: getCurrentDate(),
+            min: getCurrentDate()
           }}
         />
-        <TextField
+          <TextField
           type="time"
           label="Call Back Time"
           name="callBackTime"
@@ -283,10 +215,21 @@ const FollowUpStatus = ({ open, handleClose, rowData }) => {
             shrink: true,
           }}
         />
+
+        <TextField
+          label="Comments"
+          name="comments"
+          defaultValue={rowData.comments}
+          onChange={handleInputChange}
+          style={fieldStyle}
+          className="custom-textfield" // Apply the custom CSS class
+          multiline
+          rows={4}
+        />
       </DialogContent>
       <DialogActions>
         {loading ? (
-          <CircularProgress size={20} /> // Show loading spinner
+          <CircularProgress size={20} />
         ) : (
           <Button onClick={handleEditClick} color="primary">
             Add
@@ -294,190 +237,25 @@ const FollowUpStatus = ({ open, handleClose, rowData }) => {
         )}
       </DialogActions>
 
-      {/* Snackbar for response message */}
       <Snackbar
         open={snackbarOpen}
-        autoHideDuration={3000000} // Adjust as needed
+        autoHideDuration={3000000}
         onClose={handleSnackbarClose}
         message={responseMessage}
       />
 
-<<<<<<< HEAD
-    }
-
-
-    return (
-        <Dialog open={open} onClose={handleClose} fullWidth maxWidth="md">
-            <DialogTitle>Add to Follow Up
-
-                <IconButton
-                    color="inherit"
-                    onClick={handleClose}
-                    edge="start"
-                    aria-label="close"
-                    style={{ position: 'absolute', right: '8px', top: '8px' }}
-                >
-                    <GridCloseIcon />
-                </IconButton>
-            </DialogTitle>
-            <DialogContent>
-
-                <TextField
-                    label="Email"
-                    name="basicInfo.email"
-                    value={rowData.basicInfo.email}
-                    onChange={handleInputChange}
-                    style={fieldStyle}
-                    InputProps={{
-                        readOnly: true,
-                    }}
-                />
-                <TextField
-                    label="Name"
-                    name="basicInfo.traineeName"
-                    defaultValue={rowData.basicInfo.traineeName}
-                    style={fieldStyle}
-                    onChange={handleInputChange}
-                    InputProps={{
-                        readOnly: true,
-                    }}
-                />
-                <TextField
-                    label="Attempted By"
-                    name="attemptedBy"
-                    value={attemtedUser}
-                    defaultValue={rowData.attemptedBy}
-                    onChange={handleInputChange}
-                    style={fieldStyle}
-                    InputProps={{
-                        readOnly: true,
-                    }}
-
-                />
-                <FormControl>
-                    <InputLabel id="demo-simple-select-label">Attempt Status</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        label="Attempt Status"
-                        name="attemptStatus"
-                        onChange={handleInputChange}
-                        defaultValue={rowData.attemptStatus}
-                        variant="outlined"
-                        sx={{
-                            marginRight: '20px',
-                            width: '200px',
-                            fontSize: '20px',
-                        }}
-                    >
-                        {dropdownData.status.map((item, index) => (
-                            <MenuItem value={item} key={index}>{item}</MenuItem>
-                        ))}
-
-                    </Select>
-                </FormControl>
-
-
-                <ReactInputMask
-                    mask="99:99:99" // Define the mask pattern for hh:mm:ss
-                    value={rowData.callDuration}
-                    onChange={handleInputChange}
-                >
-                    {() => (
-                        <TextField
-                            label="Call Duration"
-                            name="callDuration"
-                            placeholder="hh:mm:ss" // Update the placeholder here
-                            variant="outlined"
-                        />
-                    )}
-                </ReactInputMask>
-
-                <TextField type="date"
-                    label="Call Back Date"
-                    name="callBack"
-                    defaultValue={rowData.callBack}
-                    onChange={handleInputChange}
-                    style={fieldStyle}
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    inputProps={{
-                        min: getCurrentDate()
-                    }}
-                />
-                <TimePicker type="time"
-                    label="Call Back Time"
-                    name="callBackTime"
-                    defaultValue={rowData.callBackTime}
-                    onChange={handleInputChange}
-                    style={fieldStyle}
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                />
-                <TextField
-                    label="Comments"
-                    name="comments"
-                    defaultValue={rowData.comments}
-                    onChange={handleInputChange}
-                    style={fieldStyle}
-                    className="custom-textfield" // Apply the custom CSS class
-                    multiline
-                    rows={4}
-                />
-            </DialogContent>
-            <DialogActions>
-                {loading ? (
-                    <CircularProgress size={20} />
-                ) : (
-                    <Button onClick={handleEditClick} color="primary">
-                        Add
-                    </Button>
-                )}
-            </DialogActions>
-
-            <Snackbar
-                open={snackbarOpen}
-                autoHideDuration={3000000}
-                onClose={handleSnackbarClose}
-                message={responseMessage}
-            />
-
-            <Dialog open={isConfirming} onClose={handleClose} fullWidth maxWidth="xs">
-                <DialogTitle>Confirm Update</DialogTitle>
-                <DialogContent>
-                    Added Follow Up Details
-                </DialogContent>
-                <DialogActions>
-                    <IconButton
-                        color="inherit"
-                        onClick={() => setIsConfirming(false)}
-                        edge="start"
-                        aria-label="close"
-                        style={{ position: 'absolute', right: '8px', top: '8px' }}
-                    >
-                        <GridCloseIcon />
-                    </IconButton>
-                    <Button onClick={handleSaveClick} color="primary">
-                        Confirm
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </Dialog>
-    );
-=======
-      {/* Confirmation Dialog */}
       <Dialog open={isConfirming} onClose={handleClose} fullWidth maxWidth="xs">
         <DialogTitle>Confirm Update</DialogTitle>
-        <DialogContent>Added Follow Up Details</DialogContent>
+        <DialogContent>
+          Added Follow Up Details
+        </DialogContent>
         <DialogActions>
           <IconButton
             color="inherit"
             onClick={() => setIsConfirming(false)}
             edge="start"
             aria-label="close"
-            style={{ position: "absolute", right: "8px", top: "8px" }}
+            style={{ position: 'absolute', right: '8px', top: '8px' }}
           >
             <GridCloseIcon />
           </IconButton>
@@ -488,7 +266,8 @@ const FollowUpStatus = ({ open, handleClose, rowData }) => {
       </Dialog>
     </Dialog>
   );
->>>>>>> 3ec4787235d7dbc41716712a5188a6fe167c9b20
+
 };
 
 export default FollowUpStatus;
+
